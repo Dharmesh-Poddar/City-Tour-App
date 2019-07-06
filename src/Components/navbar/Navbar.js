77a5ed75be4f0aaa3adb2186cca26efada3c4a97
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-06 19:16:21 +0530

    Logo of the project added

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 import './Navbar.scss';
+import logo from '../../logo.png';
 
 export default function Navbar(){
-    return <div>
-        HEllo from Navbar
+    return (
+    <nav className="navbar">
+       
+       <img src={logo} alt="city tours logo"  />
 
-    </div>
+     
+        <ul className="nav-links">
+          <li>
+              <a href="/" className="nav-link">Home </a>
+
+          </li>
+          <li>
+              <a href="/" className="nav-link">Home </a>
+              
+          </li>
+          
+          <li>
+              <a href="/" className="nav-link">Home </a>
+              
+          </li>
+        </ul>
+
+
+    </nav>
+
+
+
+    
+
+    );
+
 
 }
 
